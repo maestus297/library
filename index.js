20aa2f695764f953cd42865edd5697eff85e923b
Vishnu <a.vishnu2097@gmail.com>
2020-08-12 22:20:49 +0530

    made some fixes in the constructor

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-const by = "by";
+const by = " by ";
 const comma = ", "
-const p = "pages";
-const not = "not";
-const yet = "yet";
-
-
-
+const p = " pages";
+const not = "not ";
+const yet = " yet";
 
 let myLibrary = [];
+let i = 0;
 
-
-
-function Books(title, author, pages, read){
-    book.title = title;
-    book.author = author;
-    book.pages = pages;
-    book.read = read;
-    book.info = function() {
-        return title + by + author + comma + pages + p + comma + not + read + yet;
+function book(title, author, pages, read){
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read,
+    this.info = function() {
+        if(read.toLowerCase() == "not read") {
+            return title + by + author + comma + pages + p + comma + read.toLowerCase() + yet;
+        }
     }
 }
 
-function addBookToLibrary(var_book) {
-    
-    
+const theHobbit = new book("The Hobbit", "J.R.R. Tolkein", "295", "Not Read")
+
+function addBookToLibrary() {
+    prompt();
+    i++;
+    i+='';
+    i = new book();
+    myLibrary.push(i);
   }
-  
+
+function render () {
+}
+
+
+// create a prompt that pops up a window with the follow input options: Title, Author, Pages. 
+// And add read/not read toggle to the card. Input values should be stored in direct variables then run the constructor
