6f2f8432c339cabf02b0925040dce9d46a9dd938
Vishnu <a.vishnu2097@gmail.com>
2020-08-12 23:53:20 +0530

    worked out logic for adding items to html

--- index.js
+++ index.js
@@ -4,34 +4,44 @@ const p = " pages";
 const not = "not ";
 const yet = " yet";
 
-let myLibrary = [];
-let i = 0;
+let myLibrary = [{ Title: "The Hobbit", Author: "J.R.R. Tolkein", Pages: "295"}];
+let div, addBook, content, container, title, author, pages, read, i = 0;
 
-function book(title, author, pages, read){
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read,
-    this.info = function() {
-        if(read.toLowerCase() == "not read") {
-            return title + by + author + comma + pages + p + comma + read.toLowerCase() + yet;
-        }
+
+function book(){
+    this.Title = title,
+    this.Author = author,
+    this.Pages = pages,
+    this.Read = read
+}
+
+
+
+function addBookToLibrary() {
+    i++;
+    i+='';
+    addBook = new book();
+    myLibrary.push(addBook);
+    render();
+}
+
+function render () {
+    div = document.createElement('div');
+    container = document.querySelector('#container');
+    container.appendChild(div);
+    div.setAttribute("class", "card");
+
+    for(let key in myLibrary[i]) {
+        content = document.createElement('div');
+        div.appendChild(content); 
+        content.innerHTML = key + ": " + myLibrary[i][key];
+        console.log(key);
+        console.log(myLibrary[i][key]);
     }
+    console.log(myLibrary[i])
 }
 
 const theHobbit = new book("The Hobbit", "J.R.R. Tolkein", "295", "Not Read")
 
-function addBookToLibrary() {
-    prompt();
-    i++;
-    i+='';
-    i = new book();
-    myLibrary.push(i);
-  }
-
-function render () {
-}
-
-
-// create a prompt that pops up a window with the follow input options: Title, Author, Pages. 
-// And add read/not read toggle to the card. Input values should be stored in direct variables then run the constructor
+// create a form* that unhides when "Add Book" is pressed and has the follow input options: Title, Author, Pages. 
+// And add read/not read toggle to the card. Input values should be stored in direct variables then run the "addbookToLibrary()" function when "Submit" button is pressed.
