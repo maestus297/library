b3f41e16284c490931b9cb80a46c6f8837490fd9
Vishnu <a.vishnu2097@gmail.com>
2020-08-15 21:42:02 +0530

    added modal and form

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const yet = " yet";
 let myLibrary = [{ Title: "The Hobbit", Author: "J.R.R. Tolkein", Pages: "295"}];
 let div, addBook, content, container, title, author, pages, read, i = 0;
 
+title = document.getElementById("title").value;
+author = document.getElementById("author").value;
+pages = document.getElementById("pages").value;
+
+
 
 function book(){
     this.Title = title,
@@ -35,13 +40,34 @@ function render () {
         content = document.createElement('div');
         div.appendChild(content); 
         content.innerHTML = key + ": " + myLibrary[i][key];
-        console.log(key);
-        console.log(myLibrary[i][key]);
     }
-    console.log(myLibrary[i])
 }
 
 const theHobbit = new book("The Hobbit", "J.R.R. Tolkein", "295", "Not Read")
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
+
+
 
 // create a form* that unhides when "Add Book" is pressed and has the follow input options: Title, Author, Pages. 
 // And add read/not read toggle to the card. Input values should be stored in direct variables then run the "addbookToLibrary()" function when "Submit" button is pressed.
